85cc475093eaee54eef830435233ecd0e430c4c6
Single <libobjc@gmail.com>
2017-08-10 10:57:57 +0800

    Initial Commit

---
+++ KTVHTTPCache/KTVHTTPCache.h
@@ -0,0 +1,19 @@
+//
+//  KTVHTTPCache.h
+//  KTVHTTPCache
+//
+//  Created by Single on 2017/8/10.
+//  Copyright © 2017年 Single. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KTVHTTPCache.
+FOUNDATION_EXPORT double KTVHTTPCacheVersionNumber;
+
+//! Project version string for KTVHTTPCache.
+FOUNDATION_EXPORT const unsigned char KTVHTTPCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KTVHTTPCache/PublicHeader.h>
+
+
