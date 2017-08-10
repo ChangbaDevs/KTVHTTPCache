6c9d55ad4a812c2d4fe387030a99042dd21dbbcb
Single <libobjc@gmail.com>
2017-08-10 11:12:58 +0800

    add CocoaHTTPServer Framework

---
+++ Vendors/CocoaHTTPServer/CocoaHTTPServer/CocoaHTTPServer.h
@@ -0,0 +1,12 @@
+//
+//  CocoaHTTPServer.h
+//  CocoaHTTPServer
+//
+//  Created by Single on 2017/8/10.
+//  Copyright © 2017年 Single. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double CocoaHTTPServerVersionNumber;
+FOUNDATION_EXPORT const unsigned char CocoaHTTPServerVersionString[];
