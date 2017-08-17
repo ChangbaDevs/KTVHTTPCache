80a2b347d498792a1adb0de371a15fff712bdc79
Single <libobjc@gmail.com>
2017-08-17 11:49:45 +0800

    config log

--- KTVHTTPCache/Classes/KTVHCCommon/KTVHCLog.h
+++ KTVHTTPCache/Classes/KTVHCCommon/KTVHCLog.h
@@ -8,6 +8,83 @@
 
 #import <Foundation/Foundation.h>
 
+
+/**
+ *  Log Enable Config
+ */
+
+// HTTP Server
+
+#define KTVHCLogHTTPServerEnable                YES
+#define KTVHCLogHTTPConnectionEnable            YES
+#define KTVHCLogHTTPRequestEnable               YES
+#define KTVHCLogHTTPResponseEnable              YES
+#define KTVHCLogHTTPURLEnable                   YES
+
+// Data Storage
+
+#define KTVHCLogDataStorageEnable               YES
+#define KTVHCLogDataRequestEnable               YES
+#define KTVHCLogDataReaderEnable                YES
+
+#define KTVHCLogDataUnitEnable                  YES
+#define KTVHCLogDataUnitItemEnable              YES
+#define KTVHCLogDataUnitPoolEnable              YES
+#define KTVHCLogDataUnitQueueEnable             YES
+
+#define KTVHCLogDataSourcerEnable               YES
+#define KTVHCLogDataSourceQueueEnable           YES
+#define KTVHCLogDataFileSourceEnable            YES
+#define KTVHCLogDataNetworkSourceEnable         YES
+#define KTVHCLogDataDownloadEnable              YES
+
+// Alloc & Dealloc
+
+#define KTVHCLogAllocEnable                     YES
+#define KTVHCLogDeallocEnable                   YES
+
+
+/**
+ *  Log
+ */
+
+#if DEBUG
+#define KTVHCLog(target, enable, ...)       if (enable) {NSLog(@"%@ : %@", target, [NSString stringWithFormat:__VA_ARGS__]);}
+#else
+#define KTVHCLog(target, enable, ...)
+#endif
+
+// HTTP Server
+
+#define KTVHCLogHTTPServer(...)             KTVHCLog(@"KTVHCHTTPServer",         KTVHCLogHTTPServerEnable,         ##__VA_ARGS__)
+#define KTVHCLogHTTPConnection(...)         KTVHCLog(@"KTVHCHTTPConnection",     KTVHCLogHTTPConnectionEnable,     ##__VA_ARGS__)
+#define KTVHCLogHTTPRequest(...)            KTVHCLog(@"KTVHCHTTPRequest",        KTVHCLogHTTPRequestEnable,        ##__VA_ARGS__)
+#define KTVHCLogHTTPResponse(...)           KTVHCLog(@"KTVHCHTTPResponse",       KTVHCLogHTTPResponseEnable,       ##__VA_ARGS__)
+#define KTVHCLogHTTPURL(...)                KTVHCLog(@"KTVHCHTTPURL",            KTVHCLogHTTPURLEnable,            ##__VA_ARGS__)
+
+// Data Storage
+
+#define KTVHCLogDataStorage(...)            KTVHCLog(@"KTVHCDataStorage",        KTVHCLogDataStorageEnable,        ##__VA_ARGS__)
+#define KTVHCLogDataRequest(...)            KTVHCLog(@"KTVHCDataRequest",        KTVHCLogDataRequestEnable,        ##__VA_ARGS__)
+#define KTVHCLogDataReader(...)             KTVHCLog(@"KTVHCDataReader",         KTVHCLogDataReaderEnable,         ##__VA_ARGS__)
+
+#define KTVHCLogDataUnit(...)               KTVHCLog(@"KTVHCDataUnit",           KTVHCLogDataUnitEnable,           ##__VA_ARGS__)
+#define KTVHCLogDataUnitItem(...)           KTVHCLog(@"KTVHCDataUnitItem",       KTVHCLogDataUnitItemEnable,       ##__VA_ARGS__)
+#define KTVHCLogDataUnitPool(...)           KTVHCLog(@"KTVHCDataUnitPool",       KTVHCLogDataUnitPoolEnable,       ##__VA_ARGS__)
+#define KTVHCLogDataUnitQueue(...)          KTVHCLog(@"KTVHCDataUnitQueue",      KTVHCLogDataUnitQueueEnable,      ##__VA_ARGS__)
+
+#define KTVHCLogDataSourcer(...)            KTVHCLog(@"KTVHCDataSourcer",        KTVHCLogDataSourcerEnable,        ##__VA_ARGS__)
+#define KTVHCLogDataSourceQueue(...)        KTVHCLog(@"KTVHCDataSourceQueue",    KTVHCLogDataSourceQueueEnable,    ##__VA_ARGS__)
+#define KTVHCLogDataFileSource(...)         KTVHCLog(@"KTVHCDataFileSource",     KTVHCLogDataFileSourceEnable,     ##__VA_ARGS__)
+#define KTVHCLogDataNetworkSource(...)      KTVHCLog(@"KTVHCDataNetworkSource",  KTVHCLogDataNetworkSourceEnable,  ##__VA_ARGS__)
+#define KTVHCLogDataDownload(...)           KTVHCLog(@"KTVHCDataDownload",       KTVHCLogDataDownloadEnable,       ##__VA_ARGS__)
+
+// Alloc & Dealloc
+
+#define KTVHCLogAlloc(class, ...)           KTVHCLog(NSStringFromClass(class),   KTVHCLogAllocEnable,              ##__VA_ARGS__)
+#define KTVHCLogDealloc(class, ...)         KTVHCLog(NSStringFromClass(class),   KTVHCLogDeallocEnable,            ##__VA_ARGS__)
+
+
 @interface KTVHCLog : NSObject
 
 @end
