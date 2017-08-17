fc8f41365987d3ff98c50df0a7148e088efae0a4
Single <libobjc@gmail.com>
2017-08-17 10:17:44 +0800

    add log

---
+++ KTVHTTPCache/Classes/KTVHCCommon/KTVHCLog.h
@@ -0,0 +1,13 @@
+//
+//  KTVHCLog.h
+//  KTVHTTPCache
+//
+//  Created by Single on 2017/8/17.
+//  Copyright © 2017年 Single. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KTVHCLog : NSObject
+
+@end
---
+++ KTVHTTPCache/Classes/KTVHCCommon/KTVHCLog.m
@@ -0,0 +1,13 @@
+//
+//  KTVHCLog.m
+//  KTVHTTPCache
+//
+//  Created by Single on 2017/8/17.
+//  Copyright © 2017年 Single. All rights reserved.
+//
+
+#import "KTVHCLog.h"
+
+@implementation KTVHCLog
+
+@end
