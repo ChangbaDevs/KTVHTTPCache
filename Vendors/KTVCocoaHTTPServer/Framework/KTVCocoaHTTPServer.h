a86edd043e42373b7f6ce318bb6229f021d2b503
Single <libobjc@gmail.com>
2018-05-19 10:20:12 +0800

    replace KTVCocoaHTTPServer

---
+++ Vendors/KTVCocoaHTTPServer/Framework/KTVCocoaHTTPServer.h
@@ -0,0 +1,17 @@
+//
+//  KTVCocoaHTTPServer.h
+//  KTVCocoaHTTPServer
+//
+//  Created by Single on 2017/8/10.
+//  Copyright © 2017年 Single. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double KTVCocoaHTTPServerVersionNumber;
+FOUNDATION_EXPORT const unsigned char KTVCocoaHTTPServerVersionString[];
+
+#import <KTVCocoaHTTPServer/HTTPServer.h>
+#import <KTVCocoaHTTPServer/HTTPConnection.h>
+#import <KTVCocoaHTTPServer/HTTPMessage.h>
+#import <KTVCocoaHTTPServer/HTTPResponse.h>
