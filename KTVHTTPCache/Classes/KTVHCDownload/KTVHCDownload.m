101ad63dcf29b4adc0022ded6a3fbf2a3735f985
Single <libobjc@gmail.com>
2018-05-25 17:49:41 +0800

    download

--- KTVHTTPCache/Classes/KTVHCDownload/KTVHCDownload.m
+++ KTVHTTPCache/Classes/KTVHCDownload/KTVHCDownload.m
@@ -262,7 +262,8 @@ static UIBackgroundTaskIdentifier backgroundTaskIdentifier = -1;
         }];
         UIBackgroundTaskIdentifier blockIdentifier = backgroundTaskIdentifier;
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(300 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            if (blockIdentifier == backgroundTaskIdentifier) {
+            if (blockIdentifier == backgroundTaskIdentifier)
+            {
                 [self cleanBackgroundTask];
             }
         });
