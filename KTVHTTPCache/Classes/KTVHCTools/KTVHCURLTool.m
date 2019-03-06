091b51ea30a0fece366dc7724d04cdd926162924
Single <libobjc@gmail.com>
2019-03-06 14:39:37 +0800

    KTVHCURLTool: autoreleasepool.

--- KTVHTTPCache/Classes/KTVHCTools/KTVHCURLTool.m
+++ KTVHTTPCache/Classes/KTVHCTools/KTVHCURLTool.m
@@ -63,65 +63,67 @@
     const char *c = [URLString UTF8String];
     NSString *resultString = @"";
     for(int i = 0; i < length; i++) {
-        switch (*c) {
-            case '/':
-                resultString = [resultString stringByAppendingString:@"%2F"];
-                break;
-            case '\'':
-                resultString = [resultString stringByAppendingString:@"%27"];
-                break;
-            case ';':
-                resultString = [resultString stringByAppendingString:@"%3B"];
-                break;
-            case '?':
-                resultString = [resultString stringByAppendingString:@"%3F"];
-                break;
-            case ':':
-                resultString = [resultString stringByAppendingString:@"%3A"];
-                break;
-            case '@':
-                resultString = [resultString stringByAppendingString:@"%40"];
-                break;
-            case '&':
-                resultString = [resultString stringByAppendingString:@"%26"];
-                break;
-            case '=':
-                resultString = [resultString stringByAppendingString:@"%3D"];
-                break;
-            case '+':
-                resultString = [resultString stringByAppendingString:@"%2B"];
-                break;
-            case '$':
-                resultString = [resultString stringByAppendingString:@"%24"];
-                break;
-            case ',':
-                resultString = [resultString stringByAppendingString:@"%2C"];
-                break;
-            case '[':
-                resultString = [resultString stringByAppendingString:@"%5B"];
-                break;
-            case ']':
-                resultString = [resultString stringByAppendingString:@"%5D"];
-                break;
-            case '#':
-                resultString = [resultString stringByAppendingString:@"%23"];
-                break;
-            case '!':
-                resultString = [resultString stringByAppendingString:@"%21"];
-                break;
-            case '(':
-                resultString = [resultString stringByAppendingString:@"%28"];
-                break;
-            case ')':
-                resultString = [resultString stringByAppendingString:@"%29"];
-                break;
-            case '*':
-                resultString = [resultString stringByAppendingString:@"%2A"];
-                break;
-            default:
-                resultString = [resultString stringByAppendingFormat:@"%c", *c];
+        @autoreleasepool {
+            switch (*c) {
+                case '/':
+                    resultString = [resultString stringByAppendingString:@"%2F"];
+                    break;
+                case '\'':
+                    resultString = [resultString stringByAppendingString:@"%27"];
+                    break;
+                case ';':
+                    resultString = [resultString stringByAppendingString:@"%3B"];
+                    break;
+                case '?':
+                    resultString = [resultString stringByAppendingString:@"%3F"];
+                    break;
+                case ':':
+                    resultString = [resultString stringByAppendingString:@"%3A"];
+                    break;
+                case '@':
+                    resultString = [resultString stringByAppendingString:@"%40"];
+                    break;
+                case '&':
+                    resultString = [resultString stringByAppendingString:@"%26"];
+                    break;
+                case '=':
+                    resultString = [resultString stringByAppendingString:@"%3D"];
+                    break;
+                case '+':
+                    resultString = [resultString stringByAppendingString:@"%2B"];
+                    break;
+                case '$':
+                    resultString = [resultString stringByAppendingString:@"%24"];
+                    break;
+                case ',':
+                    resultString = [resultString stringByAppendingString:@"%2C"];
+                    break;
+                case '[':
+                    resultString = [resultString stringByAppendingString:@"%5B"];
+                    break;
+                case ']':
+                    resultString = [resultString stringByAppendingString:@"%5D"];
+                    break;
+                case '#':
+                    resultString = [resultString stringByAppendingString:@"%23"];
+                    break;
+                case '!':
+                    resultString = [resultString stringByAppendingString:@"%21"];
+                    break;
+                case '(':
+                    resultString = [resultString stringByAppendingString:@"%28"];
+                    break;
+                case ')':
+                    resultString = [resultString stringByAppendingString:@"%29"];
+                    break;
+                case '*':
+                    resultString = [resultString stringByAppendingString:@"%2A"];
+                    break;
+                default:
+                    resultString = [resultString stringByAppendingFormat:@"%c", *c];
+            }
+            c++;
         }
-        c++;
     }
     return resultString;
 }
